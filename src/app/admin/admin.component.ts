74562ae1d4fa08fed21740330c4f33ffda4ebeb8
Dave Bell <david.bell@libertymutual.com>
2017-07-25 13:07:47 -0400

    admin.event

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,  ViewChild } from '@angular/core';
+import {  DataService } from '../data.service';
+import {  NgForm } from '@angular/forms';
+import {  ActivatedRoute,  Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  users: any[];
+  user: object = {};
+  errorMessage: string;
+  successMessage: string;
+  adminOn: object = {
+    'admin': true
+  }
+
+  adminOff: object = {
+    'admin': false
+  }
+
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute
+  ) {}
+
+  getUsers() {
+    this.dataService.getRecords('getAllMembers')
+      .subscribe(
+        events => {
+          this.users = events
+          console.log(this.users)
+        });
+  }
+
+  updateUser(id, admin) {
+    console.log('Clicked ID = ' + id);
+    console.log('Admin currently = ' + admin);
+    // determine which object to send
+    let adminObj: object;
+    if (admin) {
+      adminObj = this.adminOff;
+    } else {
+      adminObj = this.adminOn;
+    }
+
+    this.dataService.editRecord('member', adminObj, id)
+      .subscribe(
+        user => this.getUsers()
+      );
+  }
 
   ngOnInit() {
+    this.getUsers();
   }
 
 }
